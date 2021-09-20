41d1dc2b1faa92dc3cd3f355e98b0bbfe374b1f9
Seedy <seedy.dupuis@gmail.com>
2021-09-20 22:08:11 +0200

    dumb: list item repository, props for config

--- components/dumb/ListItem/Repository/index.js
+++ components/dumb/ListItem/Repository/index.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-import { useCallback, useState } from 'react';
+import { forwardRef, useCallback, useState } from 'react';
 
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -10,7 +10,17 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 
 // COMPONENTS
-const ListItemRepository = ({ fullName, description, ownerAvatarUrl }) => {
+const ListItemButtonDefault = ({ fullName, ...props }) => <ListItemButton {...props} />;
+ListItemButtonDefault.propTypes = {
+  fullName: PropTypes.string,
+};
+ListItemButtonDefault.defaultProps = {
+  fullName: undefined,
+};
+
+const ListItemRepository = forwardRef(({
+  fullName, description, ownerAvatarUrl, listItemButtonComponent: ListItemButtonComponent, ...props
+}, ref) => {
   const [selectVisible, setSelectVisible] = useState(false);
 
   const onToggleVisible = useCallback(
@@ -25,8 +35,11 @@ const ListItemRepository = ({ fullName, description, ownerAvatarUrl }) => {
       onMouseEnter={onToggleVisible}
       onMouseLeave={onToggleVisible}
       alignItems="flex-start"
+      ref={ref}
+      {...props}
     >
-      <ListItemButton
+      <ListItemButtonComponent
+        fullName={fullName}
         sx={{
           border: 1,
           borderColor: 'divider',
@@ -56,15 +69,20 @@ const ListItemRepository = ({ fullName, description, ownerAvatarUrl }) => {
         >
           Select
         </Button>
-      </ListItemButton>
+      </ListItemButtonComponent>
     </ListItem>
   );
-};
+});
 
 ListItemRepository.propTypes = {
   fullName: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   ownerAvatarUrl: PropTypes.string.isRequired,
+  listItemButtonComponent: PropTypes.elementType,
+};
+
+ListItemRepository.defaultProps = {
+  listItemButtonComponent: ListItemButtonDefault,
 };
 
 export default ListItemRepository;
